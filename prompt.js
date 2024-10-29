f93b8d111e09bd15107fc1bb5ac29144e8691df0
francoseigneur__ <francoseigneur37@gmail.com>
2024-10-28 23:58:24 -0300

    Delete prompt.js

--- prompt.js
+++
@@ -1,7 +0,0 @@
-let prompt = require('prompt-sync')();
-//
-// get input from the user 
-//
-let edad = prompt('Ingrese su edad: ');
-console.log('El año que viene cumplira: ',parseInt(edad)+1)
-console.log(`El año pasado cumplio ${parseInt(edad)-1} años`) 
